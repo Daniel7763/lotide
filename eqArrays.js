6c4dbb35cdb72b6815081b635ba65967f4d5c596
Daniel Soares <danielsoares01@outlook.com>
2023-01-29 23:27:10 -0800

    adding eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,38 @@
+// assertEqual FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+// eqArrays FUNCTION
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
