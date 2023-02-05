0c4b486789329296e74b532dc998f7b698cc9592
Daniel Soares <danielsoares01@outlook.com>
2023-02-05 13:45:29 -0800

    2 files commited

--- eqObjects.js
+++ eqObjects.js
@@ -10,20 +10,22 @@ const assertEqual = function(actual, expected) {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
+// eqObjects FUNCTION
 const eqObjects = function(object1, object2) {
-
+  //check if key.length is the same
   if (Object.keys(object1).length !== Object.keys(object2).length) {
-    console.log("should not hit");
     return false;
   }
+  //cycle through keys in object1
   for (const key in object1) {
-    {
-      if (object1[key] !== object2[key]) {
-        return false;
-      }
-      
+    //check if both objects are arrays
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      // check if keys match
+    } else if (object1[key] !== object2[key]) {
+      return false;
     }
   }
+  //if all the rest is false then return true
   return true;
 };
 
