ab373541020bbf92c16c1f1296390e0d1a0dbf00
Daniel Soares <danielsoares01@outlook.com>
2023-02-20 13:31:35 -0800

    added mocha and chai to headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,26 @@
-//imported head
+//require head
 const head = require('../head');
 const assertEqual = require('../assertEqual');
 
-//head TEST CODE
-assertEqual(head([5,6,7]), 5);
+//require chai
+const assert = require('chai').assert;
 
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//head TEST CODE
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+//head CHAI TEST
+describe("#head", () => {
+
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+});
 
 //Export
 module.exports = head;
