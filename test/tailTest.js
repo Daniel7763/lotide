54914bc12651ffc6f7b39d2e032703b0efe00191
Daniel Soares <danielsoares01@outlook.com>
2023-02-20 15:34:38 -0800

    commited tailTest.js with chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,22 @@
 //require
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
+
+//chai require
+const assert = require('chai').assert;
 
 // TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+//Tail chai test code
+describe("#tail", () => {
+
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+});
+
 
 //export
 module.exports = tail;
