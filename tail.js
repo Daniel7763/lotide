62ba80967811120bd8bf400e14d7dbf24151a972
Daniel Soares <danielsoares01@outlook.com>
2023-02-19 20:48:52 -0800

    added export and require to tail.js

--- tail.js
+++ tail.js
@@ -1,21 +1,18 @@
 // FUNCTION IMPLEMENTATION
-//const assertEqual = function(actual, expected) {
-//
-// if (actual === expected) {
-//    console.log(`Assertion passed: ${actual} === ${expected}`);
-//
-//  } else {
-//
-//    console.log(`Assertion failed: ${actual} !== ${expected}`);
-//  }
-//};
+const assertEqual = function(actual, expected) {
 
-const tail = function(array) {
-  console.log(array.slice(1));
+  if (actual === expected) {
+    console.log(`Assertion passed: ${actual} === ${expected}`);
+
+  } else {
+
+    console.log(`Assertion failed: ${actual} !== ${expected}`);
+  }
 };
 
+const tail = function(array) {
+  return array.slice(1);
+};
 
-// TEST CODE
-tail(["Lighthouse Labs", "Bootcamp", "yo", "cranberries"]);
-
-tail([1, 1, 6, 7, 3, 5]);
+//export
+module.exports = tail;
